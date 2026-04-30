4241052a26fa2172a270c937b9f3b91af1901ea5
Mathéo Jensen <10632122@ucn.dk>
2026-04-30 19:00:54 +0200

    awdawd
    awdawd

---
+++ tren_webclient/example-app/resources/views/livewire-test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <livewire:workout-list />
+    @livewireScripts
+    <div>
+        <h1>Workout List</h1>
+    </div>
+</body>
+</html>
--- tren_webclient/example-app/routes/web.php
+++ tren_webclient/example-app/routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\WorkoutList;
+use Livewire\Volt\Volt;
 
 Route::view('/', 'welcome')->name('home');
+Route::get('/workouts', function () {
+    return view('livewire-test');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
